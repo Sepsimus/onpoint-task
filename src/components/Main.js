442919da4a0047dbb15a68beccc08f58b73eee34
Sepsimus <kostya2120@yandex.ru>
2021-08-22 17:32:59 +0300

    add: first page

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function Main() {
     return(
+        <div className="main__layer-1">
         <div className="main">
             <p className="main__greeting">привет,</p>
             <h2 className="main__title">это 
-                <span className="main__title main__title_type_bold"> не </span> 
-            коммерческое задание</h2>
-            <button className="main__button"> &#8594; Что дальше?</button>
+                <span className="main__title_type_bold"> не </span> 
+            коммерческое задание 
+            <button className="main__button" /></h2>
+        </div>
         </div>
     )
 }
