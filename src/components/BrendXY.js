60e75a7f2e63746e5467eb3f62d6ac378f3af9ce
Sepsimus <kostya2120@yandex.ru>
2021-08-22 22:35:02 +0300

    add: third page

---
+++ src/components/BrendXY.js
@@ -0,0 +1,31 @@
+import React from "react";
+import effectLogo from '../image/logo-1.png';
+import dataLogo from '../image/logo-2.png';
+
+function BrendXY() {
+    return(
+        <div className="brendXY">
+            <div className="brendXY__layer-1"/>
+            <h2 className="brendXY__title">КЛЮЧЕВОЕ СООБЩЕНИЕ</h2>
+            <h3 className="brendXY__product">BREND<span className="brendXY__product_type_bold">XY</span></h3>
+            <div className="brendXY__wrapper">
+                <div className="brendXY__effect">
+                    <img className="brendXY__effect-logo" src={effectLogo} alt="Логотип блока"/>
+                    <p className="brendXY__effect-text">Ehicula ipsum a arcu 
+                        cursus vitae. Eu non 
+                        diam phasellus 
+                        vestibulum lorem sed 
+                        risus ultricies
+                    </p>
+                </div>
+                <div className="brendXY__data">
+                    <img className="brendXY__data-logo" src={dataLogo} alt="Логотип блока"/>
+                    <p className="brendXY__data-text">A arcu cursus vitae</p>
+                    </div>
+                <button className="brendXY__button"></button>
+            </div>
+        </div>
+    )
+}
+
+export default BrendXY;
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,16 +3,16 @@ import Header from './Header'
 import Footer from './Footer'
 import Main from './Main'
 import Text from './Text'
-import BrandXY from './BrandXY'
+import BrendXY from './BrendXY'
 
 function Slider() {
     return(
         <>
             <Header />
             <div className="slider">
-                <Main className="slider__item" />
-                <Text className="slider__item"/>
-                {/*<BrandXY className="slider__item"/>*/}
+                {/*<Main className="slider__item" />
+                <Text className="slider__item"/>*/}
+                <BrendXY className="slider__item"/>
             </div>
             <Footer/>
         </>
